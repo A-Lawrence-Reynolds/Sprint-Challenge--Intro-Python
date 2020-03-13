1226cc741f6af04d75cb26328e6cd3e8e9e9adcf
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-03-13 10:55:51 -0700

    almost made it just need to work on the 2nd part of the file

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,10 +4,14 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
-
-    # TODO
+    # method here
+    #called drive 
+    #looks at num_wheels and returns whatever word if the len is 4
+    def drive(self):
+        return('vroooom')
+    
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -16,9 +20,16 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+# motercycle will get the GV class passed to it due to being a subclass
+# def init to be looking for 2 not 4 
+# to get info down from GV we need super to get info down into motorcycle
+class Motorcycle(GroundVehicle):
+    def __init__(self,num_wheels=2):
+        super().__init__(num_wheels)
+    def drive(self):
+        return("BRAAAP!!")
 
-# TODO
-
+    
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -29,4 +40,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for d in vehicles:
+    print(d.drive())
