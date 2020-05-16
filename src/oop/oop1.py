e82b0f2c5a841508595a1e1236b7e61b464aecfb
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-05-15 18:44:47 -0700

    finaly done with OOP

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,26 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    #base class is Vehicle 
+    pass
+
+class GroundVehicle(Vehicle):
+    #ground vehicle 
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
